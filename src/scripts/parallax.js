9c4a0125222bafb9e18ad66efa94693773dd64fb
Julia <Bubnovayn@gmail.com>
2020-05-08 21:53:39 +0600

    parallax, приветственная секция

---
+++ src/scripts/parallax.js
@@ -0,0 +1,16 @@
+const parallax = document.querySelector(".parallax");
+const layers = document.querySelectorAll(".parallax__layer");
+
+function moveLayersDependsOnScroll(wScroll) {
+  Array.from(layers).forEach((layer) => {
+    const divider = layer.dataset.speed;
+    const strate = (wScroll * divider) / 10;
+
+    layer.style.transform = `translateY(-${strate}%)`;
+  });
+}
+
+window.addEventListener("scroll", (e) => {
+  const wScroll = window.pageYOffset;
+  moveLayersDependsOnScroll(wScroll);
+});
